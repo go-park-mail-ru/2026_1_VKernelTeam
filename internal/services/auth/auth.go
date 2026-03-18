5cc91014c47158fcad6d916fa9e1e1fe83d122d4
ugryum1 <n.a.ugryumov@gmail.com>
2026-03-18 15:58:21 +0300

    rewrote the tests for internal/services/auth/auth_test.go on gomock

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -19,6 +19,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+//go:generate mockgen -source=internal/services/auth/auth.go -destination=internal/services/auth/mocks/mock_auth.go
+
 // TokenRevoker описывает интерфейс для отзыва токенов
 type TokenRevoker interface {
 	Add(jti string, exp time.Time)
