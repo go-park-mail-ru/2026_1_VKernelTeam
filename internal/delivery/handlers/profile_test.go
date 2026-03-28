07cd0ab6b0586fc8c4970f69075a5f76011a138c
ugryum1 <n.a.ugryumov@gmail.com>
2026-03-29 00:21:30 +0300

    added tests for HandleUpdateProfile

--- internal/delivery/handlers/profile_test.go
+++ internal/delivery/handlers/profile_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -8,6 +9,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-park-mail-ru/2026_1_VKernelTeam/clover/internal/domain/dto"
 	"github.com/go-park-mail-ru/2026_1_VKernelTeam/clover/internal/domain/models"
 	"github.com/go-park-mail-ru/2026_1_VKernelTeam/clover/pkg/http/middleware"
 	"github.com/golang/mock/gomock"
@@ -69,6 +71,83 @@ func TestHandleGetProfile(t *testing.T) {
 			Return(models.User{}, errors.New(ErrInternalError))
 
 		authH.HandleGetProfile(rr, req)
+
+		assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	})
+}
+
+func TestHandleUpdateProfile(t *testing.T) {
+	authH, _, mockAuth, _ := setupHandlers(t)
+
+	t.Run("Success", func(t *testing.T) {
+		userID := int64(42)
+		newName := "New Ivan Name"
+		reqBody := dto.UpdateProfileRequest{
+			Name: newName,
+		}
+		bodyBytes, _ := json.Marshal(reqBody)
+
+		// кладем ID в контекст
+		req, _ := http.NewRequest(http.MethodPatch, "/profile", bytes.NewBuffer(bodyBytes))
+		ctx := context.WithValue(req.Context(), middleware.UserIDKey, userID)
+		req = req.WithContext(ctx)
+
+		rr := httptest.NewRecorder()
+
+		updatedUser := models.User{
+			ID:   userID,
+			Name: newName,
+		}
+
+		mockAuth.EXPECT().
+			GetProfile(gomock.Any(), userID).
+			Return(updatedUser, nil)
+
+		authH.HandleGetProfile(rr, req)
+
+		assert.Equal(t, http.StatusOK, rr.Code)
+
+		var actualUser models.User
+		err := json.Unmarshal(rr.Body.Bytes(), &actualUser)
+		assert.NoError(t, err)
+		assert.Equal(t, updatedUser, actualUser)
+	})
+
+	t.Run("InvalidJSON", func(t *testing.T) {
+		userID := int64(42)
+
+		// кладем ID в контекст
+		req, _ := http.NewRequest(http.MethodPatch, "/profile", bytes.NewBuffer([]byte("{invalid}")))
+		ctx := context.WithValue(req.Context(), middleware.UserIDKey, userID)
+		req = req.WithContext(ctx)
+
+		rr := httptest.NewRecorder()
+
+		authH.HandleUpdateProfile(rr, req)
+
+		assert.Equal(t, http.StatusBadRequest, rr.Code)
+	})
+
+	t.Run("ServiceError", func(t *testing.T) {
+		userID := int64(42)
+		reqBody := dto.UpdateProfileRequest{
+			Name: "Name",
+		}
+
+		bodyBytes, _ := json.Marshal(reqBody)
+
+		// кладем ID в контекст
+		req, _ := http.NewRequest(http.MethodPatch, "/profile", bytes.NewBuffer(bodyBytes))
+		ctx := context.WithValue(req.Context(), middleware.UserIDKey, userID)
+		req = req.WithContext(ctx)
+
+		rr := httptest.NewRecorder()
+
+		mockAuth.EXPECT().
+			UpdateProfile(gomock.Any(), gomock.Any(), gomock.Any()).
+			Return(models.User{}, errors.New(ErrInternalError))
+
+		authH.HandleUpdateProfile(rr, req)
 
 		assert.Equal(t, http.StatusInternalServerError, rr.Code)
 	})
