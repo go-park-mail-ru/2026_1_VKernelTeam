f95e6e3073100e391c536c2f2e33d9de6db424e5
ugryum1 <n.a.ugryumov@gmail.com>
2026-05-05 13:24:17 +0300

    update api

--- api/docs.go
+++ api/docs.go
@@ -1925,13 +1925,37 @@ const docTemplate = `{
         "dto.ValidationErrors": {
             "type": "object",
             "properties": {
-                "email": {
+                "category_characteristics": {
                     "type": "string"
                 },
-                "name": {
+                "category_id": {
                     "type": "string"
                 },
-                "password": {
+                "custom_characteristics": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "location": {
+                    "type": "string"
+                },
+                "photos": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "price": {
+                    "type": "string"
+                },
+                "status": {
+                    "type": "string"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "user_id": {
                     "type": "string"
                 }
             }
@@ -1939,23 +1963,62 @@ const docTemplate = `{
         "models.Ad": {
             "type": "object",
             "properties": {
+                "category_characteristics": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/models.ProductCharacteristic"
+                    }
+                },
+                "category_id": {
+                    "type": "integer"
+                },
+                "created_at": {
+                    "type": "string"
+                },
+                "custom_characteristics": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/models.ProductCustomCharacteristic"
+                    }
+                },
+                "deleted_at": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "favorites_count": {
+                    "type": "integer"
+                },
                 "id": {
-                    "type": "integer",
-                    "format": "int64"
+                    "type": "integer"
+                },
+                "location": {
+                    "type": "string"
+                },
+                "photos": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
                 },
                 "price": {
-                    "type": "integer",
-                    "format": "int64"
+                    "type": "integer"
                 },
-                "sellerID": {
-                    "type": "integer",
-                    "format": "int64"
+                "seller_id": {
+                    "type": "integer"
                 },
                 "status": {
                     "type": "string"
                 },
                 "title": {
                     "type": "string"
+                },
+                "updated_at": {
+                    "type": "string"
+                },
+                "views_count": {
+                    "type": "integer"
                 }
             }
         },
@@ -1979,6 +2042,28 @@ const docTemplate = `{
                 },
                 "sort_order": {
                     "type": "integer"
+                }
+            }
+        },
+        "models.ProductCharacteristic": {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string"
+                },
+                "value": {
+                    "type": "string"
+                }
+            }
+        },
+        "models.ProductCustomCharacteristic": {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string"
+                },
+                "value": {
+                    "type": "string"
                 }
             }
         },
