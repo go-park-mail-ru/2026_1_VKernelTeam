e573dde397f9537e3a4c6d328767aab902617ae6
vir$i <egrvrb@gmail.com>
2026-05-06 13:56:30 +0300

    fix CORS for X-Device-ID

--- pkg/http/middleware/cors.go
+++ pkg/http/middleware/cors.go
@@ -24,7 +24,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Origin, Accept, X-CSRF-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Origin, Accept, X-CSRF-Token, X-Device-ID")
 		w.Header().Set("Access-Control-Expose-Headers", "X-CSRF-Token")
 		w.Header().Set("Access-Control-Max-Age", "86400")
 
