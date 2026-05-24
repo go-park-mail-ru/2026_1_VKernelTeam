2e4a8cc8bfcc34dd9a979a5273502b053f6b75b8
vir$i <egrvrb@gmail.com>
2026-05-24 18:06:21 +0300

    chore: update swagger documentation and generated API specifications

--- api/docs.go
+++ api/docs.go
@@ -3697,13 +3697,40 @@ const docTemplate = `{
         "dto.ValidationErrors": {
             "type": "object",
             "properties": {
-                "email": {
+                "category_characteristics": {
                     "type": "string"
                 },
-                "name": {
+                "category_id": {
                     "type": "string"
                 },
-                "password": {
+                "coords": {
+                    "type": "string"
+                },
+                "custom_characteristics": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string"
+                },
+                "location": {
+                    "type": "string"
+                },
+                "photos": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "price": {
+                    "type": "string"
+                },
+                "status": {
+                    "type": "string"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "user_id": {
                     "type": "string"
                 }
             }
